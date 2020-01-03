6a16df7a3560c6cee262e9ff334e20411349614f
Valeryi Savich <relrin78@gmail.com>
2020-01-03 08:46:34 +0300

    Updated used versions of umongo/motor

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from setuptools import setup
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 requirements = [
     'sanic-base-extension==0.1.1',
-    'motor==2.0.0',
-    'umongo==2.0.0',
+    'motor==2.1.0',
+    'umongo==2.2.0',
 ]
 
 
