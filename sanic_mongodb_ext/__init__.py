503145f1cc4c34285dd9aef303d75c7c3a7d9053
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2022-12-07 22:51:04 +0700

    fix deprecated setting properties on sanic v21.12+

--- sanic_mongodb_ext/__init__.py
+++ sanic_mongodb_ext/__init__.py
@@ -17,17 +17,17 @@ class MongoDbExtension(BaseExtension):
 
         lazy_instance = app.config.get('LAZY_UMONGO', None)
         if lazy_instance is not None:
-            setattr(app, self.lazy_app_attribute, lazy_instance)
+            setattr(app.ctx, self.lazy_app_attribute, lazy_instance)
 
         @app.listener('before_server_start')
         async def mongodb_configure(app_inner, _loop):
             client_options = app_inner.config.get('MONGODB_CONNECT_OPTIONS', {})
             client = AsyncIOMotorClient(app_inner.config['MONGODB_URI'], **client_options)
-            setattr(app_inner, self.app_attribute, client)
+            setattr(app_inner.ctx, self.app_attribute, client)
 
-            if not hasattr(app, 'extensions'):
-                setattr(app, 'extensions', {})
-            app.extensions[self.extension_name] = client
+            if not hasattr(app.ctx, 'extensions'):
+                setattr(app.ctx, 'extensions', {})
+            app.ctx.extensions[self.extension_name] = client
 
             database = app_inner.config.get('MONGODB_DATABASE', None)
             if lazy_instance and database:
@@ -36,7 +36,7 @@ class MongoDbExtension(BaseExtension):
 
         @app.listener('after_server_stop')
         async def mongodb_free_resources(app_inner, _loop):
-            client = getattr(app_inner, self.app_attribute, None)
+            client = getattr(app_inner.ctx, self.app_attribute, None)
 
             if client:
                 client.close()
